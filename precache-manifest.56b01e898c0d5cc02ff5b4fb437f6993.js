b8bc2ba1c37cae8dc16714eaf694103b24e12eb5
Ricardo Sifontes <jrsifontes16@gmail.com>
2020-04-15 22:05:38 -0600

    Updates

---
+++ precache-manifest.56b01e898c0d5cc02ff5b4fb437f6993.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4386cf7139f4171fc7bfd526bbdd24",
+    "url": "/rick-and-morty-app/index.html"
+  },
+  {
+    "revision": "9e87e1856c9af972f595",
+    "url": "/rick-and-morty-app/static/css/2.3b9ada37.chunk.css"
+  },
+  {
+    "revision": "92b2dca8ecd313f0cd92",
+    "url": "/rick-and-morty-app/static/css/main.44eeee95.chunk.css"
+  },
+  {
+    "revision": "9e87e1856c9af972f595",
+    "url": "/rick-and-morty-app/static/js/2.70c98c94.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rick-and-morty-app/static/js/2.70c98c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b2dca8ecd313f0cd92",
+    "url": "/rick-and-morty-app/static/js/main.18ea17de.chunk.js"
+  },
+  {
+    "revision": "dee878c050d903d215ce",
+    "url": "/rick-and-morty-app/static/js/runtime-main.78655ae3.js"
+  },
+  {
+    "revision": "7562dd8613ccd14e88b015682130eed3",
+    "url": "/rick-and-morty-app/static/media/rick_and_morty.7562dd86.svg"
+  }
+]);
